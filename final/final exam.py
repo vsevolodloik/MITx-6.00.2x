6075a28ab58745ebe122772cbc7a9812296ce86a
vsevolodloik <32078296+vsevolodloik@users.noreply.github.com>
2017-12-30 17:34:56 +0100

    Update final exam.py

--- final/final exam.py
+++ final/final exam.py
@@ -114,26 +114,3 @@ def getAverage(die, numRolls, numTrials):
 print(getAverage(Die([1,2,3,4,5,6,6,6,7]), 500, 10000))
 
 
-
-
-def find_combination(choices, total):
-    """
-    choices: a non-empty list of ints
-    total: a positive int
- 
-    Returns result, a numpy.array of length len(choices) 
-    such that
-        * each element of result is 0 or 1
-        * sum(result*choices) == total
-        * sum(result) is as small as possible
-    In case of ties, returns any result that works.
-    If there is no result that gives the exact total, 
-    pick the one that gives sum(result*choices) closest 
-    to total without going over.
-    """
-    result = [0]*len(choices)
-    for i in choices:
-        if i == total:
-            result[i] = 1
-        else:
-            
